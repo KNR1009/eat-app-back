e5e67165e7a5d36a197f8d2f0989141a7b140388
伊東和成 <itokazunari5375@icloud.com>
2021-11-15 22:42:31 +0900

    create models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,5 @@
-class CreateFood < ApplicationRecord
+class Food < ApplicationRecord
   belongs_to :restaurant
   belongs_to :order, optional: true
   has_one :line_food
-
-  # validates :name, :description, :price, presence: true
-  # validates :name, length: { maximum:30 }
-  # validates :price, numericality: { greater_than: 0}
 end
