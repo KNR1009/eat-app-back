4e84a2e50bedcc261b769b14f0bc424d58dab67a
伊東和成 <itokazunari5375@icloud.com>
2021-11-18 22:46:09 +0900

    add foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
+# Rails.application.routes.draw do
+#   namespace :api do
+#     namespace :v1 do
+#       resources :restaurants do
+#         resources :foods, only: [:index]
+#       end
+#       resources :line_foods, only: [:index, :create]
+#       put 'line_foods/replace', to: 'line_foods#replace'
+#       resources :orders, only: [:create]
+#     end
+#   end
+# end
+
 Rails.application.routes.draw do
-  # --- ここから追加 ---
   namespace :api do
     namespace :v1 do
       resources :restaurants do
         resources :foods, only: [:index]
       end
-      resources :line_foods, only: [:index, :create]
-      put 'line_foods/replace', to: 'line_foods#replace'
-      resources :orders, only: [:create]
     end
   end
-  # --- ここまで追加 ---
+
 end
